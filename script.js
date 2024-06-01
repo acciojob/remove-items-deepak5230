1cefc2f783efefd58551abfbe26aa15fc5e69f13
deepak5230 <151017553+deepak5230@users.noreply.github.com>
2024-06-01 20:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
     
-        function removeSelectedColor() {
- 
-        }
-
- function removeDropdown() {
 document.querySelector('input[type="button"][value="Select and Remove"]').addEventListener('click', function() {
     const colorSelect = document.getElementById('colorSelect');
     colorSelect.remove(colorSelect.selectedIndex);
 });
-        }
-    
